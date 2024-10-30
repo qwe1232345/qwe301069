98660c2438b0e087e614dd117de53428c2ea3be0
qwe1232345 <spakov61@gmail.com>
2024-10-30 17:10:12 +0300

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace сосал
+{
+    internal class Program
+    
+    
+    
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("сосал?");
+                string a = Console.ReadLine();
+
+                if (a == "да")
+                {
+                    Console.WriteLine("я знал!");
+                }
+
+                else if (a == "нет")
+                {
+                    Console.WriteLine("соси тогда!");
+                }
+
+                else
+                {
+                    Console.WriteLine("лож я все помню!");
+                }
+            }
+        }
+    }
+}
